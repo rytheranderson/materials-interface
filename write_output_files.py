33fd2289e5e20e73f6c5da3220a51b90b3210219
rytheranderson <43506100+rytheranderson@users.noreply.github.com>
2019-02-18 16:46:19 -0700

    Delete write_output_files.py

--- write_output_files.py
+++
@@ -1,7 +0,0 @@
-from ase.io import write
-
-def write_adsorption_configs(config_dict, filetype='cif', prefix='slab'):
-
-	for config in config_dict:
-		write(config + '.' + filetype, config_dict[config], format=filetype)
-		
